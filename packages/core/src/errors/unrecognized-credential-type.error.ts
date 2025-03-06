f98a12f73fbbf1220c51504c6d7c19fde8cbb0cf
Caleb Moore <caleb.j.moore1984@gmail.com>
2025-03-06 16:24:47 -0600

    Removed unused content

--- packages/core/src/errors/unrecognized-credential-type.error.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationError } from 'n8n-workflow';
-
-export class UnrecognizedCredentialTypeError extends ApplicationError {
-	severity = 'warning';
-
-	constructor(credentialType: string) {
-		super(`Unrecognized credential type: ${credentialType}`);
-	}
-}
--- packages/core/src/errors/unrecognized-node-type.error.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationError } from 'n8n-workflow';
-
-export class UnrecognizedNodeTypeError extends ApplicationError {
-	severity = 'warning';
-
-	constructor(packageName: string, nodeType: string) {
-		super(`Unrecognized node type: ${packageName}.${nodeType}`);
-	}
-}
